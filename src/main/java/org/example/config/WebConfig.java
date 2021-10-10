e82cf34f799e847ee305cac30a5c22ad157ffe49
Stepan Omeliukh <72635526+StepanOmeliukh@users.noreply.github.com>
2021-10-10 21:33:21 +0300

    Version 0.0.1 Start project

---
+++ src/main/java/org/example/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.example")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
